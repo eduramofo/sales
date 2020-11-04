6af75da2fdf7a4336e6a9bc6c7d1a597a370f0f3
Edu Fontes <eduramofo@gmail.com>
2020-11-04 00:42:32 -0300

    melhorias em leads

--- leads/filters.py
+++ leads/filters.py
@@ -1,15 +1,26 @@
-from leads.models import Lead
+from leads.models import Lead, LEAD_STATUS_CHOICES
 import django_filters
 
 
 class LeadFilter(django_filters.FilterSet):    
-    
+
     name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
     indicated_by_contains = django_filters.CharFilter(field_name='indicated_by', lookup_expr='icontains')
     indicated_by_exact = django_filters.CharFilter(field_name='indicated_by', lookup_expr='iexact')
     tel = django_filters.CharFilter(field_name='tel', lookup_expr='icontains')
     waid = django_filters.CharFilter(field_name='waid', lookup_expr='icontains')
 
+    # status
+    status = django_filters.ChoiceFilter(choices=LEAD_STATUS_CHOICES)
+
+    # quality
+    quality_gte = django_filters.NumberFilter(field_name='quality', lookup_expr='gte')
+    quality_lte = django_filters.NumberFilter(field_name='quality', lookup_expr='lte')
+
+    # next contact
+    next_contact_gte = django_filters.DateTimeFilter(field_name='next_contact', lookup_expr='gte')
+    next_contact_lte = django_filters.DateTimeFilter(field_name='next_contact', lookup_expr='lte')
+
     class Meta:
         model = Lead
-        fields = ['name', 'indicated_by', 'tel','waid',]
+        fields = ['name', 'status', 'indicated_by', 'tel', 'waid', 'quality', 'next_contact']
