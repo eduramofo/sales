5be9752419e8e034de9db46ffbb59ce1b7ab92ca
Eduardo Rabelo <eduramofo@gmail.com>
2020-12-08 10:40:52 -0300

    Implementa Melhorias nas actions

--- leads/templatetags/leads_extras.py
+++ leads/templatetags/leads_extras.py
@@ -74,6 +74,20 @@ def link_wise_up_link(lead_object):
 
 
 @register.simple_tag
+def link_wise_up_boleto_link(lead_object):
+
+    whatsapp_number = str(lead_object.waid)
+
+    whatsapp_api_link = 'https://api.whatsapp.com/send?phone=' + whatsapp_number
+
+    text = 'https%3A%2F%2Fwup.onl%2F0031R00002Dv1itQAB/boleto'
+
+    link_to_call = whatsapp_api_link + '&text=' + text
+
+    return link_to_call
+
+
+@register.simple_tag
 def link_whats_tentei_te_ligar(lead_object):
 
     whatsapp_number = str(lead_object.waid)
@@ -84,7 +98,7 @@ def link_whats_tentei_te_ligar(lead_object):
 
     indicated_by_first_name = str(lead_object.indicated_by).partition(' ')[0]
 
-    raw_text = 'Olá ' + lead_name + ', tudo bem? Aqui e o Eduardo e estou te contatando através da sua amiga ' + indicated_by_first_name + '. Você pode falar?'
+    raw_text = 'Olá ' + lead_name + ', tudo bem? Aqui e o Eduardo e estou te contatando através de' + indicated_by_first_name + '. Você pode falar?'
 
     text = urllib.parse.quote(raw_text)
     
@@ -100,7 +114,11 @@ def link_modelo_referido_conheci(lead_object):
 
     whatsapp_api_link = 'https://api.whatsapp.com/send?phone=' + whatsapp_number
 
-    text = 'Conheci%20um%20projeto%20para%20aprender%20ingl%C3%AAs%20da%20*Wise%20Up*%20e%20te%20indiquei.%20Acho%20que%20voc%C3%AA%20tamb%C3%A9m%20vai%20gostar!%20%F0%9F%9A%80%F0%9F%87%BA%F0%9F%87%B8%20%20O%20representante%20*Eduardo*%20da%20*Wise%20Up*%20vai%20te%20ligar%20pra%20apresentar%2C%20ok%3F%20O%20n%C3%BAmero%20dele%20%C3%A9%20esse%3A%20*(31)%2099569-1349*.%20%20Caso%20voc%C3%AA%20*N%C3%83O%20TENHA%20INTERESSE*%20em%20aprender%20Ingl%C3%AAs%20%F0%9F%91%89%20CLICA%20no%20link%20abaixo%20e%20avisa%20ele%20(dessa%20forma%20ele%20nem%20te%20liga).%20%F0%9F%91%87%20%E2%9D%8C%20http%3A%2F%2Fbit.ly%2Fwol-nao-tenho-interesse%20%20Caso%20voc%C3%AA%20*TENHA%20INTERESSE*%20em%20aprender%20Ingl%C3%AAs%20%F0%9F%91%89%20CLICA%20no%20link%20abaixo%2C%20pois%20assim%20ele%20te%20prioriza%20nos%20atendimentos.%20%F0%9F%91%87%20%E2%9C%85%20http%3A%2F%2Fbit.ly%2Fwol-tenho-interesse'
+    raw_text = """Conheci em um projeto de para aprender Inglês da *Wise Up* e te indiquei. Acho que você também vai gostar! 🚀🇺🇸.
+    O representante *Eduardo* da *Wise Up* vai te ligar para te apresentar, Ok? O número dele é esse: *(31) 99569-1349*.'
+    """
+
+    text = urllib.parse.quote(raw_text)
 
     link_to_call = whatsapp_api_link + '&text=' + text
 
@@ -114,7 +132,11 @@ def link_modelo_referido_entrei(lead_object):
 
     whatsapp_api_link = 'https://api.whatsapp.com/send?phone=' + whatsapp_number
 
-    text = 'Entrei%20em%20um%20projeto%20para%20aprender%20ingl%C3%AAs%20da%20*Wise%20Up*%20e%20te%20indiquei.%20Acho%20que%20voc%C3%AA%20tamb%C3%A9m%20vai%20gostar!%20%F0%9F%9A%80%F0%9F%87%BA%F0%9F%87%B8%20%20O%20representante%20*Eduardo*%20da%20*Wise%20Up*%20vai%20te%20ligar%20pra%20apresentar%2C%20ok%3F%20O%20n%C3%BAmero%20dele%20%C3%A9%20esse%3A%20*(31)%2099569-1349*.%20%20Caso%20voc%C3%AA%20*N%C3%83O%20TENHA%20INTERESSE*%20em%20aprender%20Ingl%C3%AAs%20%F0%9F%91%89%20CLICA%20no%20link%20abaixo%20e%20avisa%20ele%20(dessa%20forma%20ele%20nem%20te%20liga).%20%F0%9F%91%87%20%E2%9D%8C%20http%3A%2F%2Fbit.ly%2Fwol-nao-tenho-interesse%20%20Caso%20voc%C3%AA%20*TENHA%20INTERESSE*%20em%20aprender%20Ingl%C3%AAs%20%F0%9F%91%89%20CLICA%20no%20link%20abaixo%2C%20pois%20assim%20ele%20te%20prioriza%20nos%20atendimentos.%20%F0%9F%91%87%20%E2%9C%85%20http%3A%2F%2Fbit.ly%2Fwol-tenho-interesse'
+    raw_text = """Entrei em um projeto de para aprender Inglês da *Wise Up* e te indiquei. Acho que você também vai gostar! 🚀🇺🇸.
+    O representante *Eduardo* da *Wise Up* vai te ligar para te apresentar, Ok? O número dele é esse: *(31) 99569-1349*.'
+    """
+
+    text = urllib.parse.quote(raw_text)
 
     link_to_call = whatsapp_api_link + '&text=' + text
 
