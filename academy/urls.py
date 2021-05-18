f1f1f6c79bf248ce39c3145181fde590a02245f0
Edu Fontes <eduramofo@gmail.com>
2021-05-18 13:38:15 -0300

    speech na área de treinamento

--- academy/urls.py
+++ academy/urls.py
@@ -5,4 +5,12 @@ from . import views
 app_name = 'academy'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('technique/', views.technique_home, name='technique-home'),
+    path('technique/1/', views.technique_1, name='technique-1'),
+    path('technique/2/', views.technique_2, name='technique-2'),
+    path('technique/3/', views.technique_3, name='technique-3'),
+    path('technique/4/', views.technique_4, name='technique-4'),
+    path('technique/5/', views.technique_5, name='technique-5'),
+    path('technique/6/', views.technique_6, name='technique-6'),
+    path('technique/7/', views.technique_7, name='technique-7'),
 ]
--- academy/views.py
+++ academy/views.py
@@ -1,10 +1,73 @@
 from django.shortcuts import render
 
-def home(request):
 
+def home(request):
     context = {
-        'page_title': 'Academy',
+        'page_title': 'Gravações de Entrevistas',
         'nav_name': 'core_home'
     }
-
     return render(request, 'academy/index.html', context)
+
+
+def technique_home(request):
+    context = {
+        'page_title': 'Técnica Visão Geral (Big Picture)',
+        'nav_name': 'core_home'
+    }
+    return render(request, 'academy/technique/home/index.html', context)
+
+
+def technique_1(request):
+    context = {
+        'page_title': 'Abertura/Apresentação',
+        'nav_name': 'core_home'
+    }
+    return render(request, 'academy/technique/technique_1/index.html', context)
+
+
+def technique_2(request):
+    context = {
+        'page_title': 'Conexão/Investigação',
+        'nav_name': 'core_home'
+    }
+    return render(request, 'academy/technique/technique_2/index.html', context)
+
+
+def technique_3(request):
+    context = {
+        'page_title': 'Decisão Imediata (D.I.)',
+        'nav_name': 'core_home'
+    }
+    return render(request, 'academy/technique/technique_3/index.html', context)
+
+
+def technique_4(request):
+    context = {
+        'page_title': 'Speech',
+        'nav_name': 'core_home'
+    }
+    return render(request, 'academy/technique/technique_4/index.html', context)
+
+
+def technique_5(request):
+    context = {
+        'page_title': 'Fechamento',
+        'nav_name': 'core_home'
+    }
+    return render(request, 'academy/technique/technique_4/index.html', context)
+
+
+def technique_6(request):
+    context = {
+        'page_title': 'Referidos',
+        'nav_name': 'core_home'
+    }
+    return render(request, 'academy/technique/technique_4/index.html', context)
+
+
+def technique_7(request):
+    context = {
+        'page_title': 'Validação',
+        'nav_name': 'core_home'
+    }
+    return render(request, 'academy/technique/technique_4/index.html', context)
