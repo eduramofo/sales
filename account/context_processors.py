af754eda07cc74250776447cda7145d717f9fd11
Edu Fontes <eduramofo@gmail.com>
2021-11-03 22:12:27 -0300

    bug fix

--- account/context_processors.py
+++ account/context_processors.py
@@ -2,6 +2,7 @@ from account.models import Account
 
 def current_account(request):
     current_user = request.user
+    current_account_obj = None
     if current_user and not current_user.is_anonymous:
-        current_account = Account.objects.get(user=current_user)
-    return {'current_account': current_account,}
+        current_account_obj = Account.objects.get(user=current_user)
+    return {'current_account': current_account_obj,}
