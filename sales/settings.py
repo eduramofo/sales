c513487ed3b353a71792d1cabd35def0c7b6cda3
Edu Fontes <eduramofo@gmail.com>
2022-03-01 17:39:52 -0300

    deploy heroku

--- sales/settings.py
+++ sales/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'sales.wsgi.application'
 
 default_dburl = 'postgres://postgres:postgres@localhost:5432/sales'
 DATABASES = {
-    'default': config(config('DATABASE_URL_EV', default='DATABASE_URL'), default=default_dburl, cast=dburl),
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
 
